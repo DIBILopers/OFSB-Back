0e38383fce65545965a871818cb1a9f280993e5e
rnz099 <cayanan.chavez@gmail.com>
2021-06-28 23:56:16 +0800

    migration

--- database/migrations/2021_05_29_013249_create_users_table.php
+++ database/migrations/2021_05_29_013249_create_users_table.php
@@ -14,12 +14,11 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('role');
-            $table->string('pin');
+            $table->increments('id');
             $table->string('name');
-            $table->timestamps();
-          
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->nullableTimestamps();
         });
     }
 
